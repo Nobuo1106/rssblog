ad363c2a2ba1ff5cbd5067f9fdd34b3a777a9cd4
Nobuo1106 <gonnbe1986@gmail.com>
2023-06-02 07:26:12 +0900

    rssを変更

--- members.ts
+++ members.ts
@@ -9,10 +9,8 @@ export const members: Member[] = [
       "iOSエンジニアにキャリアを戻したいWEBエンジニア",
     avatarSrc: "/avatars/catnose.jpg",
     sources: [
-      "https://zenn.dev/catnose99/feed",
-      "https://catnose.medium.com/feed",
+      "https://qiita.com/gonnbe1106/feed.atom",
     ],
-    includeUrlRegex: "medium.com|zenn.dev",
     twitterUsername: "nobuoaida",
     githubUsername: "Nobuo1106",
     websiteUrl: "https://web-portfolio-nobuo-igarashi.vercel.app/",
